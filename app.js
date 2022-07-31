e0d55be479b603e81a9a52f9980b524350f67749
Sophie-ra <91337941+Sophie-ra@users.noreply.github.com>
2022-07-31 22:43:06 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ for (let  i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
 
+    
     square.addEventListener('mouseover', () =>
         setColor(square))
 
